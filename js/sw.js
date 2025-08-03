06147fe9370cf6bbfba9a776911870d5480cd42b
bitactro <ankitmishraRD@gmail.com>
2025-08-03 11:45:08 +0530

    Updated code

--- js/sw.js
+++ js/sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('bitactro-v1').then(cache=>cache.addAll(['./','./index.html'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)))});
+// self.addEventListener('install',e=>{e.waitUntil(caches.open('bitactro-v1').then(cache=>cache.addAll(['./','./index.html'])))});
+// self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)))});
